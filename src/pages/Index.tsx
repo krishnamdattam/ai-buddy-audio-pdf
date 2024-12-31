45f6623389201f7479bd0daad65cbaa008265180
Vijay Betigiri <vijay.betigiri@gmail.com>
2024-12-31 09:55:38 +0100

    Added time for each audio

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -144,8 +144,8 @@ const Index = () => {
     const hours = Math.floor(seconds / 3600);
     const minutes = Math.floor((seconds % 3600) / 60);
     return hours > 0 
-      ? `(${hours}h ${minutes}m Total)`
-      : `(${minutes} Min Total)`;
+      ? `(${hours}h ${minutes}m)`
+      : `(${minutes} Min)`;
   };
 
   const handleAudioPlay = (index: number) => {
